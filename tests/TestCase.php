0dd81b6551c7a4cd0e630a831e1a76886d7575c3
Tint Naing Win <amigo.k8@gmail.com>
2023-08-21 10:55:22 +0700

    chore:improve

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,20 +12,10 @@ class TestCase extends Orchestra
         parent::setUp();
     }
 
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             MMNameServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_myanmar-name-converter_table.php.stub';
-        $migration->up();
-        */
     }
 }
