c11d5884c2aaa2c6a0dd49e7f619fcbe0fd4f1ee
thantthuhein-genie <thantthuhein-genie@users.noreply.github.com>
2024-08-30 14:31:20 +0000

    Fix styling

--- src/MMName.php
+++ src/MMName.php
@@ -11,8 +11,8 @@ use Illuminate\Support\Traits\Macroable;
 class MMName
 {
     use EnglishSarHelpers,
-        MyanmarSarHelpers,
-        Macroable;
+        Macroable,
+        MyanmarSarHelpers;
 
     protected DataSourceDriverInterface $dataSource;
 
